6b11102979f4f763e0d54c2c68d4b2190c6f283c
Raphael409 <raphaelsimba15@gmail.com>
2021-12-30 12:16:34 +0300

    tsFile

---
+++ app.js
@@ -0,0 +1,20 @@
+// import './index.css';
+// class ticket{
+//   id : number;
+//   subject : string;
+//   Category
+// }
+// const form: HTMLFormElement = document.querySelector('#ticket');
+// // function for deleting a ticket
+// function remove(record) {
+//   alert("Do you want to delete the record?")
+// }
+// // editing a ticket record
+// function edit(record){
+// }
+// // adding a new ticket record
+// function addNew(record) {
+//   return "Do you want to add a new ticket?"
+// }
+var message = 'Hello World';
+console.log(message);
